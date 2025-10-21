e3a9fbc51b85d0898332a9a4d99c10154c75e741
Leo Lennards <leo.lukelennards@gmail.com>
2025-10-21 17:44:52 +0200

    Created the Alerts page, and linked the Alerts page to the navbar.

--- app/src/main/java/com/example/kleenpride/ui/booking/BookingActivity.kt
+++ app/src/main/java/com/example/kleenpride/ui/booking/BookingActivity.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.kleenpride.ui.theme.LimeGreen
 import com.example.kleenpride.ui.components.BottomNavBar
 
 class BookingActivity : ComponentActivity() {
